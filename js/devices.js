d1bf8017929acf010dcf62d1c6d0f4da753301fe
Yuyuan <z0975750027@gmail.com>
2024-03-07 18:01:18 +0800

    complete datatimepicker

--- js/devices.js
+++ js/devices.js
@@ -1,7 +1,12 @@
-$(document).ready(function () {
+$(function () {
     $("#header").load("header.html");
     getItem("devices", urlParams.get('groupid'));
     treeFunction();
+    $(".datatimepicker").datetimepicker({
+        timeFormat: "HH:mm",
+        dataFormat: "yy-mm-dd",
+    });
+    submitFunciton()
 });
 
 var urlParams = new URLSearchParams(window.location.search);
@@ -76,18 +81,73 @@ function buildHtml(element, items) {
     items.forEach(item => {
         switch (element) {
             case "devices":
-                html += '<div class="col-4"><ul id=' + item.objid + '><li><input class="toggle" type="checkbox" name="devices" value="' + item.objid + '">' + item.name + '</input><ul><li>Waiting...(沒東西的話重勾選一下...)</li></ul></li></ul></div>';
+                html += '<div class="col-4"><ul id=' + item.objid + '><li><input class="toggle devices" type="checkbox" name="devices" value="' + item.objid + '"><label>' + item.name + '</label></input><ul><li>Waiting...(沒東西的話重勾選一下...)</li></ul></li></ul></div>';
                 break;
             case "sensors":
-                html += '<li><input class="toggle" type="checkbox" name="sensors" value="' + item.objid + '">' + item.name + '</input><ul><li>沒資訊就等一下再重勾一次</li></ul></li>';
+                html += '<li><input class="toggle sensors" type="checkbox" name="sensors" value="' + item.objid + '"><label>' + item.name + '</label></input><ul><li>沒資訊就等一下再重勾一次</li></ul></li>';
                 break;
             case "channels":
-                html += '<li><input type="checkbox" name="channels" value="' + item.objid + '">' + item.name + '</input></li>';
+                html += '<li><input class="channels" type="checkbox" name="channels" value="' + item.objid + '">' + item.name + '</input></li>';
                 break;
         }
     })
     return html;
 }
 
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-var f1 = fso.CreateTextFile("")
+function submitFunciton() {
+    $(document).on("click", "#submit-btn", function (event) {
+        event.preventDefault();
+        // var selectedValues = [];
+        var devices={}
+        $(".devices:checked").each(function () {
+            // selectedValues.push($(this).val());
+            devices[$(this).val()]={deviceName:$(this).parent().children("label").text()};
+            var sensors = {}
+            $(this).find('input.sensors:checked').each(function() {
+                var sensorId = $(this).val();
+                var sensorName = $(this).closest('li').text().trim();
+                console.log(sensorId+"1");
+                // var channels = [];
+                // $(this).siblings('ul').find('input.channels:checked').each(function() {
+                //     channels.push($(this).val());
+                // });
+                // if (channels.length > 0) {
+                //     sensors[sensorId] = { sensorName: sensorName, channels: channels };
+                // }
+            });
+        });
+        console.log(devices);
+    });
+}
+// async function test() {
+//     // 请求用户授权访问文件系统
+//     async function requestFileSystemAccess() {
+//         try {
+//             const directoryHandle = await window.showDirectoryPicker();
+//             return directoryHandle;
+//         } catch (error) {
+//             console.error('Failed to request file system access:', error);
+//         }
+//     }
+
+//     // 创建文件夹
+//     async function createDirectory(directoryHandle, directoryName) {
+//         try {
+//             await directoryHandle.getDirectoryHandle(directoryName, { create: true });
+//             console.log('Directory successfully created.');
+//         } catch (error) {
+//             console.error('Failed to create directory:', error);
+//         }
+//     }
+
+//     // 当用户点击按钮时触发创建文件夹操作
+//     document.getElementById('requestAccessButton').addEventListener('click', async () => {
+//         const directoryHandle = await requestFileSystemAccess();
+//         if (directoryHandle) {
+//             const directoryName = 'NewFolder';
+//             await createDirectory(directoryHandle, directoryName);
+//         }
+//     });
+// }
+
+
